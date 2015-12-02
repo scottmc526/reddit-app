c1324461eb3f0325782ebab546e1267150e4a9e1
Scott Christensen <scottmc526@gmail.com>
2015-12-02 13:36:54 -0700

    housekeeping

--- index2.js
+++ index2.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 var search;
   $('#submit').click(function() {
     search = $('#search_term').val();
+    search1 = $('#exclude').val();
 
   var getter = $.ajax ({
     url: "https://www.reddit.com/subreddits/search.json?q="+search,
@@ -10,11 +11,12 @@ var search;
   });
 
   getter.done(function(returned) {
+    var reject = "cannot display"
     for (var i = 0; i < returned['data']['children'].length; i++) {
         var inside = returned['data']['children'][i]['data']['title'];
-        var insider = returned['data']['children'][i]['data']['subscribers'];
-        $('#tabl').append('<tr><td>'+inside+'</td></tr>')
-        $('#tabl').append('<tr><td>'+insider+'</td></tr>')
+        var insider = returned['data']['children'][i]['data']['url'];
+          $('#tabl').append('<tr><td>'+inside+'</td></tr>');
+          $('#tabl').append('<tr><td>'+insider+'</td></tr>');
       }
     })
   })
